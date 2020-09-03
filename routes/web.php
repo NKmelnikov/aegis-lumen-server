210c93b2be3abf2ce5fcc0b45fefdae0ef331c78
Nkmelnikov <Nkmelnikov@hotmail.com>
2020-09-03 22:58:06 +0300

    auth is not necessary for get requests

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,17 @@ $router->group(["prefix" => "auth"], function () use ($router) {
  * Routes for categories and there items
  */
 $router->post("/ck-upload", "Admin\UploadController@ckUpload");
-
+$router->get("/get-brands", "Admin\BrandController@getAll");
+$router->post("/get-brand-by-slug", "Admin\BrandController@getBySlug");
+$router->get("/get-catalogs", "Admin\CatalogController@getAll");
+$router->get("/get-posts", "Admin\NewsController@getAll");
+$router->get("/get-categories", "Admin\CategoryController@getAll");
+$router->post("/get-category-by-id", "Admin\CategoryController@getById");
+$router->get("/get-subcategories", "Admin\SubcategoryController@getAll");
+$router->post("/get-by-category-id", "Admin\SubcategoryController@getByCategoryId");
+$router->get("/get-products-oil", "Admin\ProductsOilController@getAll");
+$router->post("/get-product-oil-by-slug", "Admin\ProductsOilController@getBySlug");
+$router->get("/get-products-drill", "Admin\ProductsDrillController@getAll");
 
 $router->group(
     [
@@ -25,8 +35,6 @@ $router->group(
         $router->post("/upload-img-from-b64", "Admin\UploadController@uploadFromBase64");
         $router->post("/upload-pdf", "Admin\UploadController@uploadPdf");
 //        brands
-        $router->get("/get-brands", "Admin\BrandController@getAll");
-        $router->post("/get-brand-by-slug", "Admin\BrandController@getBySlug");
         $router->post("/create-brand", "Admin\BrandController@create");
         $router->post("/update-brand", "Admin\BrandController@update");
         $router->post("/delete-brand", "Admin\BrandController@delete");
@@ -35,7 +43,6 @@ $router->group(
         $router->post("/bulk-deactivate-brands", "Admin\BrandController@bulkDeactivate");
         $router->post("/bulk-delete-brands", "Admin\BrandController@bulkDelete");
 //        catalogs
-        $router->get("/get-catalogs", "Admin\CatalogController@getAll");
         $router->post("/create-catalog", "Admin\CatalogController@create");
         $router->post("/update-catalog", "Admin\CatalogController@update");
         $router->post("/delete-catalog", "Admin\CatalogController@delete");
@@ -44,7 +51,6 @@ $router->group(
         $router->post("/bulk-deactivate-catalogs", "Admin\CatalogController@bulkDeactivate");
         $router->post("/bulk-delete-catalogs", "Admin\CatalogController@bulkDelete");
 //        news
-        $router->get("/get-posts", "Admin\NewsController@getAll");
         $router->post("/create-post", "Admin\NewsController@create");
         $router->post("/update-post", "Admin\NewsController@update");
         $router->post("/delete-post", "Admin\NewsController@delete");
@@ -53,8 +59,6 @@ $router->group(
         $router->post("/bulk-deactivate-posts", "Admin\NewsController@bulkDeactivate");
         $router->post("/bulk-delete-posts", "Admin\NewsController@bulkDelete");
 //        categories
-        $router->get("/get-categories", "Admin\CategoryController@getAll");
-        $router->post("/get-category-by-id", "Admin\CategoryController@getById");
         $router->post("/create-category", "Admin\CategoryController@create");
         $router->post("/update-category", "Admin\CategoryController@update");
         $router->post("/delete-category", "Admin\CategoryController@delete");
@@ -63,8 +67,6 @@ $router->group(
         $router->post("/bulk-deactivate-categories", "Admin\CategoryController@bulkDeactivate");
         $router->post("/bulk-delete-categories", "Admin\CategoryController@bulkDelete");
 //        subcategories
-        $router->get("/get-subcategories", "Admin\SubcategoryController@getAll");
-        $router->post("/get-by-category-id", "Admin\SubcategoryController@getByCategoryId");
         $router->post("/create-subcategory", "Admin\SubcategoryController@create");
         $router->post("/update-subcategory", "Admin\SubcategoryController@update");
         $router->post("/delete-subcategory", "Admin\SubcategoryController@delete");
@@ -73,8 +75,6 @@ $router->group(
         $router->post("/bulk-deactivate-subcategories", "Admin\SubcategoryController@bulkDeactivate");
         $router->post("/bulk-delete-subcategories", "Admin\SubcategoryController@bulkDelete");
 //        products_oil
-        $router->get("/get-products-oil", "Admin\ProductsOilController@getAll");
-        $router->post("/get-product-oil-by-slug", "Admin\ProductsOilController@getBySlug");
         $router->post("/create-product-oil", "Admin\ProductsOilController@create");
         $router->post("/update-product-oil", "Admin\ProductsOilController@update");
         $router->post("/delete-product-oil", "Admin\ProductsOilController@delete");
@@ -83,7 +83,6 @@ $router->group(
         $router->post("/bulk-deactivate-products-oil", "Admin\ProductsOilController@bulkDeactivate");
         $router->post("/bulk-delete-products-oil", "Admin\ProductsOilController@bulkDelete");
 //        products_drill
-        $router->get("/get-products-drill", "Admin\ProductsDrillController@getAll");
         $router->post("/create-product-drill", "Admin\ProductsDrillController@create");
         $router->post("/update-product-drill", "Admin\ProductsDrillController@update");
         $router->post("/delete-product-drill", "Admin\ProductsDrillController@delete");
