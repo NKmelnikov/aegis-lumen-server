adeae5e38a9b193a04384a128ef080811ddcdb3f
Nkmelnikov <Nkmelnikov@hotmail.com>
2020-09-22 14:15:03 +0300

    getOilProductBySlug enlarged

--- app/Services/Admin/ProductsOilService.php
+++ app/Services/Admin/ProductsOilService.php
@@ -47,8 +47,33 @@ class ProductsOilService extends BaseService
     public function getBySlug(Request $request)
     {
         $slug = json_decode($request['slug']);
+
+        $productsOil = DB::table('products_oil as  p')
+            ->leftJoin('brands as b', 'p.brand_id', '=', 'b.id')
+            ->leftJoin('categories as c', 'p.category_id', '=', 'c.id')
+            ->leftJoin('subcategories as s', 'p.subcategory_id', '=', 's.id')
+            ->select('p.id as id',
+                'p.brand_id',
+                'p.category_id',
+                'p.subcategory_id',
+                'b.name as brand_name',
+                'c.name as category_name',
+                's.name as subcategory_name',
+                'p.active',
+                'p.position',
+                'p.name',
+                'p.slug',
+                'p.description',
+                'p.spec',
+                'p.imgPath',
+                'p.pdf1Path',
+                'p.pdf2Path',
+                'p.created_at')
+            ->where('p.slug', $slug)
+            ->first();
+
         try {
-            return response()->json(ProductsOil::where('slug', $slug)->first());
+            return response()->json($productsOil);
         } catch (Exception $e) {
             return response()->json(["message" => $e->getMessage()], 400);
         }
