a8e0f957df4c9d2929f69326b74941668403b45d
Nkmelnikov <Nkmelnikov@hotmail.com>
2020-12-11 17:09:18 +0300

    clear frola .4

--- app/Services/Admin/ProductsOilService.php
+++ app/Services/Admin/ProductsOilService.php
@@ -155,6 +155,6 @@ class ProductsOilService extends BaseService
     }
 
     private function clearFrolaMessage($text) {
-        return str_replace('<p data-f-id=\"pbf\" style=\"text-align: center; font-size: 14px; margin-top: 30px; opacity: 0.65; font-family: sans-serif;\">Powered by <a href=\"https://www.froala.com/wysiwyg-editor?pb=1\" title=\"Froala Editor\">Froala Editor</a></p>', '', $text);
+        return str_replace('<p data-f-id="pbf" style="text-align: center; font-size: 14px; margin-top: 30px; opacity: 0.65; font-family: sans-serif;">Powered by <a href="https://www.froala.com/wysiwyg-editor?pb=1" title="Froala Editor">Froala Editor</a></p>', '', $text);
     }
 }
